ee145a2353f3e8c5603e3011278f067ae73a1c84
snowmaniowa <santharaam@gmail.com>
2018-01-13 10:54:50 -0600

    File upload sample program and modules

---
+++ demo8_upload.js
@@ -0,0 +1,31 @@
+/**
+* There is a very good module for working with file uploads, called "Formidable".
+* STOP: Run "npm install formidable" before working on this program
+*/
+
+//Import module for formidable
+var formidable = require('formidable');
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/dev/sandbox/nodejs/uploadedfiles/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
